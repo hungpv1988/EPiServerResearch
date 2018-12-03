277966380c6af069d58feaad6679ba3ff954eb38
hung phan <hung.phan@episerver.com>
2018-12-03 16:39:23 +0700

    init the site

---
+++ Business/IModifyLayout.cs
@@ -0,0 +1,13 @@
+using EPiServerResearch.Models.ViewModels;
+
+namespace EPiServerResearch.Business
+{
+    /// <summary>
+    /// Defines a method which may be invoked by PageContextActionFilter allowing controllers
+    /// to modify common layout properties of the view model.
+    /// </summary>
+    interface IModifyLayout
+    {
+        void ModifyLayout(LayoutModel layoutModel);
+    }
+}
---
+++ Models/Pages/ISearchPage.cs
@@ -0,0 +1,9 @@
+namespace EPiServerResearch.Models.Pages
+{
+    /// <summary>
+    /// Marker interface for search implementation
+    /// </summary>
+    public interface ISearchPage
+    {
+    }
+}
