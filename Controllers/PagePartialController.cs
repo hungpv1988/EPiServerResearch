c5eddc0f0ea2a20c1ea34d6ee91bd00527af6f0f
hung phan <hung.phan@episerver.com>
2018-12-03 17:44:33 +0700

    add partial page rendering

---
+++ Controllers/PagePartialController.cs
@@ -0,0 +1,22 @@
+﻿using EPiServer.Framework.DataAnnotations;
+using EPiServer.Web.Mvc;
+using EPiServerResearch.Models.Pages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EPiServerResearch.Controllers
+{
+    [TemplateDescriptor(Inherited = true)]
+    public class PagePartialController : PartialContentController<SitePageData>
+    {
+        //
+        // GET: /PagePartial/
+        public override ActionResult Index(SitePageData currentContent)
+        {
+            return PartialView("/Views/Shared/PagePartials/Page.cshtml", currentContent);
+        }
+    }
+}
